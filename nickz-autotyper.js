fd53d53af7d2c6336c4cc476222e3492124fc1ac
Nickz <yudimatheusmartins@gmail.com>
2025-10-17 12:20:20 -0300

    Update nickz-autotyper.js

--- nickz-autotyper.js
+++ nickz-autotyper.js
@@ -123,7 +123,7 @@
   modal.id = 'nickz-autotyper-modal';
   modal.innerHTML = `
     <div id="nickz-autotyper-header">
-      <span>✍️ AutoTyper</span>
+      <span>✍️ Nickz AutoTyper by Nickz (Yudi)</span>
       <button id="nickz-minimize-btn">−</button>
     </div>
     <div id="nickz-autotyper-body">
